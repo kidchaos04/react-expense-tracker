2d018cf9e02ea32fa7f3845fa8a13f88074630b9
kidchaos04 <vinayakrastogi94@gmail.com>
2020-05-06 22:45:34 +0530

    Updates

---
+++ precache-manifest.5347dbf4a4ced23c0f29caf1ed3d398d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b4aa4c0e06aff8775d7ad10705731b4",
+    "url": "/react-expense-tracker/index.html"
+  },
+  {
+    "revision": "ae3e2de5e97810a632d6",
+    "url": "/react-expense-tracker/static/css/main.e508d319.chunk.css"
+  },
+  {
+    "revision": "b3d44f0b3a5040cb70a9",
+    "url": "/react-expense-tracker/static/js/2.915b108d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-expense-tracker/static/js/2.915b108d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae3e2de5e97810a632d6",
+    "url": "/react-expense-tracker/static/js/main.6102f86b.chunk.js"
+  },
+  {
+    "revision": "b620a0abcfb146992cec",
+    "url": "/react-expense-tracker/static/js/runtime-main.53cc54ef.js"
+  }
+]);
